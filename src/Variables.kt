d8f6e8b9118db2474f0b6cfc9a59928e266c75f4
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2023-10-11 20:08:42 +0530

    Kotlin Essentials Start

---
+++ src/Variables.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var score: Int = 12
+    var temperature: Double = 80.99
+    var isRaining: Boolean = false
+    var alphabet: Char = 'A'
+    var message: String = "Hello World"
+}
