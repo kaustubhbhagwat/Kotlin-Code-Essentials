53b6b97ee6c9ec9d9eb110f3d9b62d19e99a6934
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2023-10-26 17:20:00 +0530

    for loop

--- src/Loops.kt
+++ src/Loops.kt
@@ -16,4 +16,25 @@ fun main() {
     do {
         println("Hello this is do-while")
     }while (index2>5)
+
+    // upper bound included
+    for (i in 1..5 step 2){
+        println(i)
+    }
+
+    //upper bound not included
+    for(i in 1 until 5){
+        println(i)
+    }
+
+    for(i in 10 downTo 1 step 2){
+        println(i)
+    }
+
+    val value = 2
+    for(i in 1..10){
+//        println(value.toString() + "x" + i +"=" + (value * i))
+        //string templating
+        println("$value + x $i = ${value*i}")
+    }
 }
