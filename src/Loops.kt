3c97c6e7322e09e7b8fa3e50f3d5a3342c9cf927
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2023-10-26 13:50:48 +0530

    do-while

---
+++ src/Loops.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var count =5
+    while(count>=1){
+        println("Hello kb")
+        count --
+    }
+
+    var number = 2
+    var index = 1
+    while (index<=10){
+        println(number * index)
+        index++
+    }
+
+    var index2 = 5
+    do {
+        println("Hello this is do-while")
+    }while (index2>5)
+}
