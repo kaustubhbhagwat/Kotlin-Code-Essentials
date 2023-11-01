2b379663df0f2f44da8278ff1b40097b6ff8ac36
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2023-11-01 16:09:57 +0530

    classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,5 +1,10 @@
 fun main() {
 
+    var i :Int = 20
+
+    println(i.plus(20))
+    println(i.toFloat())
+
     val mustang = Car("Mustang","Petrol", 200)
     val beetle = Car("beetle","Petrol", 300)
 
@@ -9,6 +14,17 @@ fun main() {
 
     println(beetle.applyBrakes(beetle.name))
 
+    val p1 = Person("A",21)
+    val p2 = Person("B",12)
+
+    println(p1.canVote())
+    println(p2.canVote())
+}
+
+class Person(val name: String, var age: Int){
+    fun canVote(): Boolean{
+        return age > 18
+    }
 }
 
 class Car(val name: String , val type: String, val kmRan: Int){
