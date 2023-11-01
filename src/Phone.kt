9cf314a3d53a7d563351a3e6aee3da79239f35eb
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2023-11-01 19:26:59 +0530

    Inheritance

---
+++ src/Phone.kt
@@ -0,0 +1,12 @@
+open class Phone {
+
+    val name: String = ""
+    val type: String = ""
+    val volume: Int = 10
+
+
+    fun makeCall(){}
+    fun display(){}
+    fun powerOff(){}
+    fun getDeviceInfo(){}
+}
---
+++ src/inheritance.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val objChild = Child()
+
+//    objChild.myMethod()
+//    objChild.myMethod2()
+
+}
+
+open class Parent{
+
+    init {
+        println("Parent Constructor us called")
+    }
+    val name: String = ""
+    fun myMethod(){
+        println("I am Parent")
+
+    }
+}
+
+class Child: Parent(){
+
+    init {
+        println("Child Constructor us called")
+    }
+    val name2: String =""
+    fun myMethod2(){
+        println("I am a Child")
+    }
+}
