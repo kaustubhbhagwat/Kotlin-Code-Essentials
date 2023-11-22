ce879b99a543c8806766098366a91fa0d6a44de9
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2023-11-22 17:59:27 +0530

    lambdas

--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -3,6 +3,11 @@ import kotlin.math.pow
 fun main(){
 
     println(sum(2.0,3.0))
+    println(power1(2.0,3.0))
+
+    var fn : (a: Double, b: Double) -> Double = ::sum
+    println(fn(2.5,3.0))
+    calculator(5.0,6.0, ::sum)
 }
 
 fun sum( a:Double, b: Double):Double{
@@ -12,3 +17,9 @@ fun sum( a:Double, b: Double):Double{
 fun power1(a:Double, b:Double): Double{
     return a.pow(b)
 }
+
+fun calculator(a: Double, b: Double, gn: (Double,Double)->Double){
+    val result = gn(a,b)
+    println(result)
+}
+
