83e9ddf80c76ec2af58eefccecedfd16a085f273
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-05-01 19:43:16 +0530

    Lambdas basic

--- src/lambdas/LambdaExample.kt
+++ src/lambdas/LambdaExample.kt
@@ -9,7 +9,7 @@ val multiply: (Int,Int) ->Int = {a:Int,b:Int -> a*b}
 val divide: (Int,Int) ->Int= {a:Int,b:Int -> a/b }
 
 fun main() {
-    println(add(1,2))
+    println(add.invoke(1,2))
     println(subtract(1,2))
     println(multiply(1,2))
     println(divide(1,2))
