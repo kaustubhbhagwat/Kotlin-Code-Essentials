b830db19bff7fcc3fa7096a457f21c8781000262
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-05-03 13:05:20 +0530

    Lambdas shorthand and class extension

--- src/lambdas/LambdaExample.kt
+++ src/lambdas/LambdaExample.kt
@@ -12,6 +12,15 @@ val square: (Int)-> Int = { it * it }
 
 val cube :( Int)->Int = {it * it * it}
 
+val myVariable : (Int, String) -> String = {a: Int, b: String -> "$a +$b"}
+
+//Shorter syntax of 2 types
+val myVariable1  = {a: Int, b: String -> "$a +$b"}
+
+val myVariable2 : (Int, String) -> String = {a, b -> "$a +$b"}
+
+
+
 val lambdaAbbreviate: String.() -> String ={
     val splitString = this.split(" "," ", ignoreCase = true)
     var abbrevition: StringBuilder? = StringBuilder("")
@@ -28,4 +37,9 @@ fun main() {
     println(divide(1,2))
     println("Square of the number is ${square(5)}")
     println(lambdaAbbreviate("Kaustubh Bhagwat"))
+    println(myVariable(1,"A"))
+    println(myVariable1(1,"A"))
+    println(myVariable2(1,"A"))
+
+
 }
